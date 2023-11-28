b1565f5fbba70fe12ed99b0be2bd4b887d64bfb7
Phillip Bailey <ztron500@gmail.com>
2023-11-28 18:13:23 -0500

    Update page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -13,13 +13,11 @@ return (
 <h1 className="about-title">About Me</h1>
 <h1 className="main-clock" id="clock"><Clock/></h1>
 </div>
-<p>Hi there, and thanks for stopping by! My name is Phillip Andrew Bailey and I am a Front-End Developer and UX/UI designer based in LaFayette, Georgia. My hobbies consist of video games, animal documentries, anime cartoons, coding, and listening to old-school rnb. I hold a bachelor's and associate's degree in computer science from Southern New Hampshire University, where I spent 5 years studying and learning about programming, Technology, and computer systems.</p>
+<p>Hi there, and thanks for stopping by! My name is Phillip Andrew Bailey and I am a Front-End Developer and UX/UI designer based in LaFayette, Georgia. My hobbies comprise video games, wild-life documentaries, anime cartoons, coding, and listening to old-school rnb music. I hold a bachelor's and associate's degree in computer science from Southern New Hampshire University, where I spent 5 years studying and learning about programming, technology, and computer systems.</p>
   
-<p> With a background in Information Technology/Web Design and a passion for creating intuitive and visually appealing user experiences, I have had the opportunity to work on a wide range of projects using languages like Angular, React JS, Next Js.
-My design philosophy revolves around the idea that form should follow function, and I strive to create designs that are not only aesthetically pleasing but also highly functional and efficient. </p>
+<p> With a background in IT/Web Design and a love for creating user-friendly and visually pleasing experiences, I've worked on diverse projects using Angular, React JS, Next Js. My design philosophy revolves around the idea that form should follow function, and I strive to create designs that are not only aesthetically pleasing, but also highly functional and efficient. </p>
   
-<p>I am skilled in a variety of design tools including Affinity designer, Affinity Photo, Figma.
-In my free time, you can find me staying up-to-date on the latest design trends, tinkering with new design tools, and collaborating with other designers on personal projects. Thank you for visiting my website and please feel free to reach out if you have any questions or would like to hire me.</p>
+<p>I am skilled in a variety of design tools including affinity designer, affinity Photo, Figma. In my free time, you can find me staying up-to-date on the latest design trends, tinkering with new design tools, and collaborating with other designers on personal projects. Thank you for visiting my website and please reach out if you have questions or would like to hire me.</p>
 
 <p><b>Kind Regards</b> <br /> Phillip Andrew Bailey</p>
 <hr
