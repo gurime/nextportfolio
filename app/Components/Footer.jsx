e1af02d50785d31837e40270ad99e10194542b13
Phillip Bailey <ztron500@gmail.com>
2023-12-20 18:31:54 -0500

    update

--- app/Components/Footer.jsx
+++ app/Components/Footer.jsx
@@ -13,16 +13,14 @@ window.open('https://www.linkedin.com/in/ogphillip-bailey/', '_blank');
 return (
 <>
 <footer>
-<div className="footer-container">
-<div className="social-links-box">
 <p>&#169; Created by Phillip Andrew Bailey</p>
+
 <div style={{marginRight:'1rem',display:'flex'}}>
 <Image height={24} width={24} src={linkedin} onClick={linkedinhandleClick}   alt=""/>
 <div style={{padding: '0 5px'}}></div>
 <Image height={24} width={24} onClick={gitHubhandleClick} src={githutimg} alt=""/>
 </div>
-</div>
-</div>
+
 </footer>
 </>
 )
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -7,7 +7,7 @@ import Footer from '../Components/Footer'
 
 export const metadata = {
   title: 'Phillip Bailey - Contact',
-  description: 'Get in touch with Phillip Andrew Bailey, a Front-End Developer and UX/UI designer based in LaFayette, Georgia. Explore contact information, including phone number, address, and email. Reach out for collaboration or job interview!',
+  description: 'Phillip Andrew Bailey, a Front-End Developer and UX/UI designer based in LaFayette, Georgia. Explore contact information, including phone number, address, and email. Reach out for collaboration or job interview!',
   keywords: 'Contact, Phillip Andrew Bailey, Front-End Developer, UX/UI designer, LaFayette, Georgia, Phone Number, Address, Email, Collaboration'
 }
 
