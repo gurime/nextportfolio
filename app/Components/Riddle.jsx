030f1ebf565ab2777c135d4b7bad7b96281b77c6
Phillip Bailey <ztron500@gmail.com>
2024-08-05 17:24:53 -0400

    Update Riddle.jsx

--- app/Components/Riddle.jsx
+++ app/Components/Riddle.jsx
@@ -11,7 +11,7 @@ const [isAnswer3Visible, setAnswer3Visibility] = useState(false);
 return (
 <>
 <div className="container">
-<h1 className="faq-title" >Contact Information</h1>
+<h1 className='password-title'>Contact Information</h1>
 {/**first question block */}
 <div className="question">
 <h2>Contact Number</h2>
